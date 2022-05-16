6886cfcf516538ab7bfffaf037ba8c91a34f36df
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-05-16 16:24:22 +0200

    Vežbe 9 - drugi deo

--- src/app/components/artikl/artikl.component.ts
+++ src/app/components/artikl/artikl.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 import { Artikl } from 'src/app/models/artikl';
 import { ArtiklService } from 'src/app/services/artikl.service';
+import { ArtiklDialogComponent } from '../dialogs/artikl-dialog/artikl-dialog.component';
 
 @Component({
   selector: 'app-artikl',
@@ -11,11 +13,12 @@ import { ArtiklService } from 'src/app/services/artikl.service';
 })
 export class ArtiklComponent implements OnInit, OnDestroy {
 
-  displayedColumns = ['id', 'naziv', 'proizvojdac', 'actions'];
+  displayedColumns = ['id', 'naziv', 'proizvodjac', 'actions'];
   dataSource!: MatTableDataSource<Artikl>;
   subscription!: Subscription;
 
-  constructor(private artiklService: ArtiklService) { }
+  constructor(private artiklService: ArtiklService, 
+    private dialog: MatDialog) { }
   
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
@@ -27,12 +30,24 @@ export class ArtiklComponent implements OnInit, OnDestroy {
 
   public loadData() {
     this.subscription = this.artiklService.getAllArtikls().subscribe(data => {
-      console.log(data);
+     // console.log(data);
+     this.dataSource = new MatTableDataSource(data);
     },
     (error:Error) => {
       console.log(error.name + ' ' + error.message);
     }
     );
+  }
+
+  public openDialog(flag: number, id?:number, naziv?:string, proizvodjac?:string) {
+    const dialogRef = this.dialog.open(ArtiklDialogComponent, {data: {id,naziv,proizvodjac}});
+  
+    dialogRef.componentInstance.flag = flag;
+    dialogRef.afterClosed().subscribe(result => {
+      if(result == 1) {
+        this.loadData();
+      }
+    })
   }
 
 }
